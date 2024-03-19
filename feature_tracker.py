c5aed5f874a3a2774847945faec88811a14fe0c5
unknown <e0543690@u.nus.edu>
2024-03-19 14:06:01 +0800

    Huge revamp

--- feature_tracker.py
+++ feature_tracker.py
@@ -1,31 +1,123 @@
+## Refer to: https://scikit-learn.org/stable/auto_examples/tree/plot_unveil_tree_structure.html#sphx-glr-auto-examples-tree-plot-unveil-tree-structure-py
+## To understand sklearn's underlying tree structure
+
 from sklearn.ensemble import GradientBoostingRegressor
-class RegressorTreeProcessor:
-    def __init__(self, gb_regressor):
-        if not isinstance(gb_regressor, GradientBoostingRegressor):
-            raise ValueError("Input must be a GradientBoostingRegressor instance.")
-        self.gb_regressor = gb_regressor
-        self.trees = gb_regressor.estimators_
+import pandas as pd
 
-    def get_tree(self, tree_index):
-        if tree_index < 0 or tree_index >= len(self.trees):
-            raise ValueError(f"Tree index must be between 0 and {len(self.trees) - 1}.")
-        return self.trees[tree_index][0]
 
-    def predict_with_tree(self, tree_index, X):
-        tree = self.get_tree(tree_index)
-        return tree.predict(X)
+class CustomGBR(GradientBoostingRegressor):
+    def __init__(self, loss='squared_error', learning_rate=0.1, n_estimators=100, subsample=1.0, criterion='friedman_mse', 
+                 min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0., max_depth=3, min_impurity_decrease=0., 
+                 init=None, random_state=None, max_features=None, alpha=0.9, verbose=0, max_leaf_nodes=None, warm_start=False, 
+                 validation_fraction=0.1, n_iter_no_change=None, tol=0.0001, ccp_alpha=0.0):
+        super().__init__(
+        loss=loss,
+        learning_rate=learning_rate,
+        n_estimators=n_estimators,
+        criterion=criterion,
+        min_samples_split=min_samples_split,
+        min_samples_leaf=min_samples_leaf,
+        min_weight_fraction_leaf=min_weight_fraction_leaf,
+        max_depth=max_depth,
+        init=init,
+        subsample=subsample,
+        max_features=max_features,
+        min_impurity_decrease=min_impurity_decrease,
+        random_state=random_state,
+        alpha=alpha,
+        verbose=verbose,
+        max_leaf_nodes=max_leaf_nodes,
+        warm_start=warm_start,
+        validation_fraction=validation_fraction,
+        n_iter_no_change=n_iter_no_change,
+        tol=tol,
+        ccp_alpha=ccp_alpha)
+        self.iter_feature_importances_ = []
+        self.detailed_impact_ = []
+
+    def fit(self, X, y):
+        self.iter_feature_importances_ = []
+
+        # Call GBR's fit
+        super().fit(X, y)
+        
+        # Save feature importances after each estimator
+        for estimator in self.estimators_:
+            stage_importances = estimator[0].tree_.compute_feature_importances(normalize=False)
+            self.iter_feature_importances_.append(stage_importances)
+        
+        self.iter_feature_impacts_ = self.aggregate_feature_impacts()
+
+        return self
     
-    def track_tree_prediction(self, tree_index, X):
-        tree = self.get_tree(tree_index)
-        node_values = []
-        node = tree.tree_
-        current_node = 0
-        while node.children_left[current_node] != node.children_right[current_node]:
-            feature_index = node.feature[current_node]
-            threshold = node.threshold[current_node]
-            if X[0, feature_index] <= threshold:
-                current_node = node.children_left[current_node]
-            else:
-                current_node = node.children_right[current_node]
-            node_values.append((feature_index, threshold))
-        return node_values
+
+    def get_feature_importance_per_iteration(self):
+        # Each row corresponds to an iteration, each column to a feature
+        importances_df = pd.DataFrame(self.iter_feature_importances_)
+        return importances_df
+    
+
+    def get_feature_impacts_per_iteration(self):
+        return self.aggregate_feature_impacts()
+
+
+    def aggregate_feature_impacts(self):
+        feature_impacts = []
+        for iter, estimator in enumerate(self.estimators_):
+            for split in self._extract_splits(estimator[0].tree_, iteration=iter + 1):
+                feature_impacts.append(split)
+        
+        df = pd.DataFrame(feature_impacts, columns=['iteration', 'feature', 'threshold', 'impact', 'samples'])
+        return df
+    
+
+    def _extract_splits(self, tree, node_id=0, iteration=0, path=[]):
+        # Check for leaf nodes
+        if tree.children_left[node_id] == tree.children_right[node_id] == -1:
+            return []
+
+        # Check node's split details; not a leaf node
+        feature_index = tree.feature[node_id]
+        threshold = tree.threshold[node_id]
+        left_child_node_id, right_child_node_id = tree.children_left[node_id], tree.children_right[node_id]
+        
+        # Calculate the impact of a feature split, defined as value of left child - value of right child
+        impact = (tree.value[right_child_node_id].sum() - tree.value[left_child_node_id].sum()) * self.learning_rate
+
+        # Get sample sizes of the splits
+        left_child_sample_size, right_child_sample_size = tree.n_node_samples[left_child_node_id], tree.n_node_samples[right_child_node_id]
+
+        splits_info = [{
+            'iteration': iteration,
+            'feature': feature_index,
+            'threshold': threshold,
+            'impact': impact, # Represents the magnitude of change from low to high values of a particular feature in that iteration
+            'samples': left_child_sample_size + right_child_sample_size
+        }]
+
+        # Recursively get split information for the left sub-nodes
+        if tree.children_left[node_id] != -1:
+            left_path = path + [f'{feature_index} <= {threshold}']
+            splits_info += self._extract_splits(tree, tree.children_left[node_id], iteration, left_path)
+
+        # Recursively get split information for the right sub-nodes
+        if tree.children_right[node_id] != -1:
+            right_path = path + [f'{feature_index} > {threshold}']
+            splits_info += self._extract_splits(tree, tree.children_right[node_id], iteration, right_path)
+
+        return splits_info
+
+
+if __name__ == "__main__":
+    import numpy as np
+    import pandas as pd
+    from sklearn.utils import check_array
+    from sklearn.datasets import load_diabetes
+    from sklearn.model_selection import train_test_split
+    import matplotlib.pyplot as plt
+    diabetes_data = load_diabetes()
+    X_data, y_data = diabetes_data.data, diabetes_data.target
+    X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.1, random_state=42)
+    gbr = CustomGBR(n_estimators=10, learning_rate=0.1, max_depth=3)
+    gbr.fit(X_train, y_train)
+    feature_impacts_per_iter = gbr.get_feature_impacts_per_iteration()
